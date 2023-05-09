b5b53ee4bb92b33fb383245e887fa98cbcd76bc8
yaryna-bashchak <y.bashchak.im11@kpi.ua>
2023-05-09 23:15:13 +0300

    add models for relations Many-To-Many

--- API/Data/CourseContext.cs
+++ API/Data/CourseContext.cs
@@ -11,6 +11,28 @@ namespace API.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<LessonKeyword>()
+                .HasKey(lk => new { lk.LessonId, lk.KeywordId });  
+            modelBuilder.Entity<LessonKeyword>()
+                .HasOne(lk => lk.Lesson)
+                .WithMany(l => l.LessonKeywords)
+                .HasForeignKey(lk => lk.LessonId);  
+            modelBuilder.Entity<LessonKeyword>()
+                .HasOne(lk => lk.Keyword)
+                .WithMany(k => k.LessonKeywords)
+                .HasForeignKey(lk => lk.KeywordId);
+
+            modelBuilder.Entity<CourseLesson>()
+                .HasKey(cl => new { cl.CourseId, cl.LessonId });  
+            modelBuilder.Entity<CourseLesson>()
+                .HasOne(cl => cl.Course)
+                .WithMany(c => c.CourseLessons)
+                .HasForeignKey(cl => cl.CourseId);
+            modelBuilder.Entity<CourseLesson>()
+                .HasOne(cl => cl.Lesson)
+                .WithMany(l => l.CourseLessons)
+                .HasForeignKey(cl => cl.LessonId);
+
             modelBuilder.Entity<Test>()
                 .HasOne(t => t.Lesson)
                 .WithMany(l => l.Tests);
@@ -19,9 +41,25 @@ namespace API.Data
                 .HasOne(o => o.Test)
                 .WithMany(t => t.Options);
 
+            modelBuilder.Entity<LessonPreviousLesson>()
+                .HasKey(lpl => new { lpl.LessonId, lpl.PreviousLessonId });
+            modelBuilder.Entity<LessonPreviousLesson>()
+                .HasOne(lpl => lpl.Lesson)
+                .WithMany(l => l.PreviousLessons) // <--
+                .HasForeignKey(lpl => lpl.LessonId)
+                .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<LessonPreviousLesson>()
+                .HasOne(lpl => lpl.PreviousLesson)
+                .WithMany(pl => pl.PreviousLessonOf) // <--
+                .HasForeignKey(lpl => lpl.PreviousLessonId);        
         }
 
         public DbSet<Lesson> Lessons { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<CourseLesson> CourseLessons { get; set; }
+        public DbSet<LessonPreviousLesson> PreviousLessons { get; set; }
+        public DbSet<Keyword> Keywords { get; set; }
+        public DbSet<LessonKeyword> LessonKeywords { get; set; }
         public DbSet<Test> Tests { get; set; }
         public DbSet<Option> Options { get; set; }
     }
