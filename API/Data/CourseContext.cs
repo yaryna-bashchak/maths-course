9363d6b55458258f5f400881b2e0ef598fe82c8b
yaryna-bashchak <y.bashchak.im11@kpi.ua>
2023-05-09 23:14:49 +0300

    add models for relations One-To-Many

--- API/Data/CourseContext.cs
+++ API/Data/CourseContext.cs
@@ -9,6 +9,20 @@ namespace API.Data
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Test>()
+                .HasOne(t => t.Lesson)
+                .WithMany(l => l.Tests);
+
+            modelBuilder.Entity<Option>()
+                .HasOne(o => o.Test)
+                .WithMany(t => t.Options);
+
+        }
+
         public DbSet<Lesson> Lessons { get; set; }
+        public DbSet<Test> Tests { get; set; }
+        public DbSet<Option> Options { get; set; }
     }
 }
