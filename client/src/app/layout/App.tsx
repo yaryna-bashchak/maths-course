df2c2a124b0c77d661ce5e2ee5569182637a9955
yaryna-bashchak <y.bashchak.im11@kpi.ua>
2023-06-21 17:56:31 +0300

    create Header component using MUI

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { Lesson } from "../models/lesson";
 import ListOfLessons from "../../features/listOfLessons/ListOfLessons";
+import { Container, CssBaseline } from "@mui/material";
+import Header from "./Header";
 
 function App() {
   const [lessons, setLessons] = useState<Lesson[]>([]);
@@ -13,28 +15,28 @@ function App() {
 
   function addLesson() {
     setLessons(prevState => [...prevState,
-      {
-        id: prevState.length + 1,
-        title: 'Тема ' + (prevState.length + 1),
-        description: 'На уроці ви дізнаєтеся про те і те.',
-        urlTheory: "",
-        urlPractice: "string",
-        number: prevState.length + 1,
-        importance: 1,
-        isCompleted: false,
-        keywords: [
-          {
-            id: 6,
-            word: "кут"
-          },
-        ],
-      }])
+    {
+      id: prevState.length + 1,
+      title: 'Тема ' + (prevState.length + 1),
+      description: 'На уроці ви дізнаєтеся про те і те.',
+      urlTheory: "",
+      urlPractice: "string",
+      number: prevState.length + 1,
+      importance: 1,
+      isCompleted: false,
+      keywords: [
+        {
+          id: 6,
+          word: "кут"
+        },
+      ],
+    }])
   }
 
   return (
     <>
-      <h1>План ZNO-шника</h1>
-      <ListOfLessons lessons={lessons} addLesson={addLesson} />
+      <CssBaseline />
+      <Header />
     </>
   );
 }
