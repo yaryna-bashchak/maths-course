2c145bebbc5798e02013c0a1a1ba20a5f082f5c8
yaryna-bashchak <y.bashchak.im11@kpi.ua>
2023-06-21 18:07:56 +0300

    fix spaces

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -5,43 +5,43 @@ import { Container, CssBaseline } from "@mui/material";
 import Header from "./Header";
 
 function App() {
-  const [lessons, setLessons] = useState<Lesson[]>([]);
+    const [lessons, setLessons] = useState<Lesson[]>([]);
 
-  useEffect(() => {
-    fetch('http://localhost:5000/api/lessons')
-      .then(response => response.json())
-      .then(data => setLessons(data))
-  }, [])
+    useEffect(() => {
+        fetch('http://localhost:5000/api/lessons')
+            .then(response => response.json())
+            .then(data => setLessons(data))
+    }, [])
 
-  function addLesson() {
-    setLessons(prevState => [...prevState,
-    {
-      id: prevState.length + 1,
-      title: 'Тема ' + (prevState.length + 1),
-      description: 'На уроці ви дізнаєтеся про те і те.',
-      urlTheory: "",
-      urlPractice: "string",
-      number: prevState.length + 1,
-      importance: 1,
-      isCompleted: false,
-      keywords: [
+    function addLesson() {
+        setLessons(prevState => [...prevState,
         {
-          id: 6,
-          word: "кут"
-        },
-      ],
-    }])
-  }
+            id: prevState.length + 1,
+            title: 'Тема ' + (prevState.length + 1),
+            description: 'На уроці ви дізнаєтеся про те і те.',
+            urlTheory: "",
+            urlPractice: "string",
+            number: prevState.length + 1,
+            importance: 1,
+            isCompleted: false,
+            keywords: [
+                {
+                    id: 6,
+                    word: "кут"
+                },
+            ],
+        }])
+    }
 
-  return (
-    <>
-      <CssBaseline />
-      <Header />
-      <Container sx={{ pt: "70px" }}>
-        <LessonList lessons={lessons} addLesson={addLesson} />
-      </Container>
-    </>
-  );
+    return (
+        <>
+            <CssBaseline />
+            <Header />
+            <Container sx={{ pt: "70px" }}>
+                <LessonList lessons={lessons} addLesson={addLesson} />
+            </Container>
+        </>
+    );
 }
 
 export default App;
--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -4,107 +4,107 @@ import { useState } from "react";
 import SearchIcon from '@mui/icons-material/Search';
 
 const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(3),
-    width: 'auto',
-  },
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(3),
+        width: 'auto',
+    },
 }));
 
 const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: '20ch',
+        },
     },
-  },
 }));
 
 export default function Header() {
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
-  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
+    const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
-  return (
-    <AppBar position="fixed" sx={{ top: 0, bottom: "auto" }}>
-      <Toolbar>
-        <Avatar alt="Logo" src="/images/header/logo.jpg" sx={{ mr: "10px" }} />
-        <Typography variant="h6">
-          План ZNO-шника
-        </Typography>
-        <Search>
-          <SearchIconWrapper>
-            <SearchIcon />
-          </SearchIconWrapper>
-          <StyledInputBase
-            placeholder="Знайти..."
-            inputProps={{ 'aria-label': 'search' }}
-          />
-        </Search>
-        <Box sx={{ flexGrow: 1 }} />
-        <IconButton
-          size="large"
-          aria-label="account of current user"
-          aria-controls="menu-appbar"
-          aria-haspopup="true"
-          onClick={handleMenu}
-          color="inherit"
-        >
-          <AccountCircle />
-          <Typography variant="body1" sx={{ ml: "5px" }}>
-            Яринка
-          </Typography>
-        </IconButton>
-        <Menu
-          id="menu-appbar"
-          anchorEl={anchorEl}
-          anchorOrigin={{
-            vertical: 'top',
-            horizontal: 'right',
-          }}
-          keepMounted
-          transformOrigin={{
-            vertical: 'top',
-            horizontal: 'right',
-          }}
-          open={Boolean(anchorEl)}
-          onClose={handleClose}
-        >
-          <MenuItem onClick={handleClose}>Профіль</MenuItem>
-          <MenuItem onClick={handleClose}>Мої Курси</MenuItem>
-          <MenuItem onClick={handleClose}>Всі Курси</MenuItem>
-          <MenuItem onClick={handleClose}>Вийти</MenuItem>
-        </Menu>
-      </Toolbar>
-    </AppBar>
-  )
+    return (
+        <AppBar position="fixed" sx={{ top: 0, bottom: "auto" }}>
+            <Toolbar>
+                <Avatar alt="Logo" src="/images/header/logo.jpg" sx={{ mr: "10px" }} />
+                <Typography variant="h6">
+                    План ZNO-шника
+                </Typography>
+                <Search>
+                    <SearchIconWrapper>
+                        <SearchIcon />
+                    </SearchIconWrapper>
+                    <StyledInputBase
+                        placeholder="Знайти..."
+                        inputProps={{ 'aria-label': 'search' }}
+                    />
+                </Search>
+                <Box sx={{ flexGrow: 1 }} />
+                <IconButton
+                    size="large"
+                    aria-label="account of current user"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    onClick={handleMenu}
+                    color="inherit"
+                >
+                    <AccountCircle />
+                    <Typography variant="body1" sx={{ ml: "5px" }}>
+                        Яринка
+                    </Typography>
+                </IconButton>
+                <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                    open={Boolean(anchorEl)}
+                    onClose={handleClose}
+                >
+                    <MenuItem onClick={handleClose}>Профіль</MenuItem>
+                    <MenuItem onClick={handleClose}>Мої Курси</MenuItem>
+                    <MenuItem onClick={handleClose}>Всі Курси</MenuItem>
+                    <MenuItem onClick={handleClose}>Вийти</MenuItem>
+                </Menu>
+            </Toolbar>
+        </AppBar>
+    )
 }
