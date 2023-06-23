2816875fa07730e3988701b2cda5ecd6c285bc62
yaryna-bashchak <y.bashchak.im11@kpi.ua>
2023-06-23 18:38:46 +0300

    fix mistake

---
+++ API/Dtos/Keyword/UpdateKeywordDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Dtos.Keyword
+{
+    public class UpdateKeywordDto
+    {
+        public string Word { get; set; }
+    }
+}
