a3a840c2be22d696c2fdd61c04df197de3dc7ada
yaryna-bashchak <y.bashchak.im11@kpi.ua>
2023-06-30 19:30:34 +0300

    use axios instead of fetch

--- client/src/features/course/CourseDetails.tsx
+++ client/src/features/course/CourseDetails.tsx
@@ -2,25 +2,34 @@ import { List, Typography } from "@mui/material";
 import SectionItem from "./SectionItem";
 import { useEffect, useState } from "react";
 import { Course, Section } from "../../app/models/course";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 export default function CourseDetails() {
     const [course, setCourse] = useState<Course>();
     const [sections, setSections] = useState<Section[]>([]);
+    const [loading, setLoading] = useState(true);
+    const {id} = useParams<{id: string}>();
 
     useEffect(() => {
-        fetch('http://localhost:5000/api/courses/1')
-            .then(response => response.json())
-            .then(data => {
-                setCourse(data);
-                setSections(data.sections)
+        axios.get(`http://localhost:5000/api/courses/${id}`)
+            .then(response => {
+                setCourse(response.data);
+                setSections(response.data.sections)
             })
-    }, [])
+            .catch(error => console.log(error))
+            .finally(() => setLoading(false));
+    }, [id])
 
     const [openIndex, setOpenIndex] = useState(-1);
 
     const handleItemClick = (index: number) => {
         setOpenIndex(openIndex !== index ? index : -1);
     };
+
+    if (loading) return <h3>Loading...</h3>
+    
+    if (!course) return <h3>Course not found</h3>
 
     return (
         <>
--- client/src/features/lesson/LessonDetails.tsx
+++ client/src/features/lesson/LessonDetails.tsx
@@ -3,35 +3,36 @@ import { useState, useEffect } from "react";
 import { Lesson } from "../../app/models/lesson";
 import Tests from "./Tests";
 import Videos from "./Videos";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 export default function LessonDetails() {
-    const [lesson, setLesson] = useState<Lesson>();
+    const [lesson, setLesson] = useState<Lesson | null>(null);
     const [isTheoryCompleted, setIsTheoryCompleted] = useState<boolean>(false);
     const [isPracticeCompleted, setIsPracticeCompleted] = useState<boolean>(false);
+    const [loading, setLoading] = useState(true);
     // let testScore = -1;
-    const id = 1;
+    const {id} = useParams<{id: string}>();
 
+    
     useEffect(() => {
-        fetch('http://localhost:5000/api/lessons/1')
-            .then(response => response.json())
-            .then(data => {
-                setLesson(data);
-                if (data) {
-                    setIsTheoryCompleted(data.isTheoryCompleted);
-                    setIsPracticeCompleted(data.isPracticeCompleted);
-                } else {
-                    console.log(`No lesson found with id ${id}`);
-                }
+        axios.get(`http://localhost:5000/api/lessons/${id}`)
+            .then(response => {
+                setLesson(response.data);
+                setIsTheoryCompleted(response.data.isTheoryCompleted);
+                setIsPracticeCompleted(response.data.isPracticeCompleted);
             })
-    }, [])
-
+            .catch(error => console.log(error))
+            .finally(() => setLoading(false));
+    }, [id])
+    
     useEffect(() => {
         if (lesson) {
             const data = {
                 isTheoryCompleted: Number(isTheoryCompleted),
                 isPracticeCompleted: Number(isPracticeCompleted)
             };
-
+            
             fetch(`http://localhost:5000/api/Lessons/${lesson.id}`, {
                 method: 'PUT',
                 headers: {
@@ -53,11 +54,14 @@ export default function LessonDetails() {
     const handleChangeTheory = (event: React.ChangeEvent<HTMLInputElement>) => {
         setIsTheoryCompleted(event.target.checked);
     };
-
+    
     const handleChangePractice = (event: React.ChangeEvent<HTMLInputElement>) => {
         setIsPracticeCompleted(event.target.checked);
     };
-
+    
+    if (loading) return <h3>Loading...</h3>
+    
+    if (!lesson) return <h3>Lesson not found</h3>
 
     return (
         lesson ? (
