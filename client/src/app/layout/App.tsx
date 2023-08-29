019812123d6bc395ec3787813042050ba2dc3b4f
yaryna-bashchak <yaryna.bashchak@gmail.com>
2023-08-29 18:27:26 +0300

    add routes for login and register

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,10 +1,14 @@
 import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import Header from "./Header";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css"
 
 function App() {
+    const location = useLocation();
+    const whiteBackgroundNotNeeded = ["/login", "/register"]
+    const isWhiteBackgroundNeeded = !whiteBackgroundNotNeeded.includes(location.pathname);
+    
     const theme = createTheme({
         palette: {
             background: {
@@ -16,20 +20,20 @@ function App() {
     return (
         <>
             <ThemeProvider theme={theme}>
-                <ToastContainer position="bottom-right" theme="colored"/>
+                <ToastContainer position="bottom-right" theme="colored" />
                 <CssBaseline />
                 <Header />
                 <Container sx={{
                     pt: "90px",
                     pb: "20px",
                     width: "100%",
-                    backgroundColor: "white",
+                    backgroundColor: isWhiteBackgroundNeeded ? "white" : "none",
                     [theme.breakpoints.up('md')]: {
                         width: '70%',
                         // mawWidth: "1200px",
                     },
                 }}>
-                    <Outlet />
+                <Outlet />
                 </Container>
             </ThemeProvider >
         </>
