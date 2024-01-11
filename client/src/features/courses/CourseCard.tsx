a42ae7d58fefd82b05e130e6b23457627dbfa5e3
yaryna-bashchak <yaryna.bashchak@gmail.com>
2024-01-11 16:03:18 +0200

    make buttons to be different for every stage of course

--- client/src/features/courses/CourseCard.tsx
+++ client/src/features/courses/CourseCard.tsx
@@ -10,9 +10,21 @@ interface Props {
 export default function CourseCard({ courseId }: Props) {
     const course = useAppSelector(state => courseSelectors.selectById(state, courseId!));
 
+    type Stage = "notBought" | "boughtInPart" | "boughtInFull";
+
+    const stageOfCourse = (): Stage => {
+        if (course?.sections.every(value => value.isAvailable === true)) {
+            return "boughtInFull";
+        } else if (course?.sections.some(value => value.isAvailable === true)) {
+            return "boughtInPart";
+        } else {
+            return "notBought";
+        }
+    }
+
     return (
         <>
-            <Card sx={{ width: '90%', maxWidth: '300px', minWidth: '250px'}}>
+            <Card sx={{ width: '90%', maxWidth: '300px', minWidth: '250px' }}>
                 <CardMedia
                     sx={{ height: 140 }}
                     image="https://lifeimg.pravda.com/images/doc/8/2/8252751-vipdesignusa-depositphotos.jpg"
@@ -31,8 +43,23 @@ export default function CourseCard({ courseId }: Props) {
                     </Typography>
                 </CardContent>
                 <CardActions sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-                    <Button component={Link} to={`/course/${course?.id}`} size="small">Дізнатись більше</Button>
-                    <Button size="small" variant="contained">Купити</Button>
+                    <Button component={Link} to={`/course/${course?.id}`} size="small">
+                        {stageOfCourse() === "notBought" ? "Теми уроків" : "Проходити курс"}
+                    </Button>
+                    <Button
+                        size="small"
+                        variant={stageOfCourse() === "boughtInFull" ? "outlined" : "contained"}
+                        // color="success"
+                        disabled={stageOfCourse() === "boughtInFull"}
+                        style={{
+                            backgroundColor: stageOfCourse() === "boughtInFull" ? '#f0f7f0' : '',
+                            borderColor: stageOfCourse() === "boughtInFull" ? '#89b38c' : '',
+                            color: stageOfCourse() === "boughtInFull" ? '#6da670' : '',
+                            opacity: 1
+                        }}
+                        >
+                        {stageOfCourse() === "notBought" ? "Купити" : stageOfCourse() === "boughtInPart" ? "Купити наступний місяць" : "Куплено"}
+                    </Button>
                 </CardActions>
             </Card >
         </>
