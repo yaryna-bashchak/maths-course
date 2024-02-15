1f1ca137ff7ef4f91ad5f44de9e4542bef72c541
yaryna-bashchak <yaryna.bashchak@gmail.com>
2024-02-15 14:10:17 +0200

    get roles from token on client side

--- client/src/features/account/accountSlice.ts
+++ client/src/features/account/accountSlice.ts
@@ -55,7 +55,13 @@ export const accountSlice = createSlice({
       router.navigate('/')
     },
     setUser: (state, action) => {
-      state.user = action.payload
+      const claims = JSON.parse(atob(action.payload.token.split('.')[1]))
+      const roles =
+        claims['http://schemas.microsoft.com/ws/2008/06/identity/claims/role']
+      state.user = {
+        ...action.payload,
+        roles: typeof roles === 'string' ? [roles] : roles
+      }
     }
   },
   extraReducers: builder => {
@@ -68,7 +74,13 @@ export const accountSlice = createSlice({
     builder.addMatcher(
       isAnyOf(singInUser.fulfilled, fetchCurrentUser.fulfilled),
       (state, action) => {
-        state.user = action.payload
+        const claims = JSON.parse(atob(action.payload.token.split('.')[1]))
+        const roles =
+          claims['http://schemas.microsoft.com/ws/2008/06/identity/claims/role']
+        state.user = {
+          ...action.payload,
+          roles: typeof roles === 'string' ? [roles] : roles
+        }
       }
     )
     builder.addMatcher(isAnyOf(singInUser.rejected), (_state, action) => {
