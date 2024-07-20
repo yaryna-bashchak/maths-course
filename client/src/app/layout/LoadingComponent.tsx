159854075ac709e5d2db4de6538a37002972f5e8
yaryna-bashchak <yaryna.bashchak@gmail.com>
2024-07-20 21:42:24 +0300

    improve loading component

--- client/src/app/layout/LoadingComponent.tsx
+++ client/src/app/layout/LoadingComponent.tsx
@@ -2,15 +2,21 @@ import { Backdrop, Box, CircularProgress, Typography } from "@mui/material";
 
 type Props = {
     message?: string;
+    fullScreen?: boolean;
 };
 
-export default function LoadingComponent({ message = 'Завантаження...' }: Props) {
-    return (
-        <Backdrop open={true} invisible={true}>
-            <Box display="flex" justifyContent="center" alignItems="center" height="100vh">
-                <CircularProgress size={80} color="secondary" />
-                <Typography variant="h5" sx={{ justifyContent: "center", position: "fixed", top: "60%" }}>{message}</Typography>
-            </Box>
-        </Backdrop>
-    )
+export default function LoadingComponent({
+    message = 'Завантаження...',
+    fullScreen = true,
+}: Props) {
+    const loadingContent = (
+        <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" height={fullScreen ? '100vh' : '100%'} >
+            <CircularProgress size={80} color="secondary" />
+            <Typography variant="h5" sx={{ justifyContent: 'center', marginTop: 2 }}>
+                {message}
+            </Typography>
+        </Box>
+    );
+
+    return fullScreen ? <Backdrop open={true} invisible={true}>{loadingContent}</Backdrop> : loadingContent;
 }
